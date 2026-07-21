15fdded18cde833e6e90ea9739173b5243496433
jooyong.lee <jooyong.lee@medit.com>
2026-07-21 04:27:47 +0000

    predict코드 수정

--- rfdetr/deploy/predict.py
+++ rfdetr/deploy/predict.py
@@ -1,4 +1,6 @@
 import os
+import glob
+import cv2
 import ast
 import random
 import argparse
@@ -20,9 +22,9 @@ from rfdetr.models import build_model
 # from rfdetr.deploy._onnx import OnnxOptimizer
 import re
 import sys
+from rfdetr import RFDETRBase, RFDETRNano, RFDETRSmall
 
 from rfdetr.config import (
-    RFDETRBaseConfig,
     RFDETRLargeConfig,
     RFDETRNanoConfig,
     RFDETRSmallConfig,
@@ -32,6 +34,8 @@ from rfdetr.config import (
     SegmentationTrainConfig,
     ModelConfig
 )
+
+from rfdetr.engine import draw_preditions_boxes
 
 def main():
     # print("git:\n  {}\n".format(utils.get_sha()))
@@ -44,39 +48,10 @@ def main():
     # else:
     #     device_id = str(int(args.device))
     
-    from rfdetr.main import populate_args
+    # from rfdetr.main import populate_args
     #     args.device = f"cuda:{device_id}"
+    rf_detr = RFDETRSmall(
         
-    # rf_detr = RFDETRSmall(
-    
-    #     patch_size=16,
-    #     num_windows=4,
-    #     # num_queries=100,
-    #     num_queries=100,
-    #     group_detr=5,
-    #     num_select=30,
-    #     encoder='dinov2_windowed_tiny',
-    #     # encoder='dinov2_windowed_base',
-        
-    #     # patch_size=24,
-    #     # num_channels=1,
-    #     # eval=True,
-    #     # num_classes=32,
-    #     num_classes=num_classes,
-    #     segmentation_head=args.segmentation_head,
-    #     # pretrain_weights="output/checkpoint0099.pth",
-    #     # pretrain_weights="output/xray_teeth/checkpoint0059.pth",
-    #     # pretrain_weights='output/xray_teeth33/checkpoint0039.pth'
-    #     # pretrain_weights='output/xray_teeth33/checkpoint_best_regular.pth'
-    #     # pretrain_weights='output/xray_teeth33_nano/checkpoint_best_total.pth'
-    #     # pretrain_weights='output/xray_teeth33_small/checkpoint_best_regular.pth'
-    #     # pretrain_weights= args.pretrain_weights # 'output/xray_teeth33_small_seg/checkpoint0039.pth',
-    #     # pretrain_weights='output/xray_teeth33_dinov2tiny_small/checkpoint0039.pth',
-    #     pretrain_weights='output/xray_teeth33_dinov2tiny_small_seg/checkpoint0499.pth'
-        
-    # )
-        
-    config = RFDETRBaseConfig(
         patch_size=16,
         num_windows=4,
         # num_queries=100,
@@ -84,28 +59,76 @@ def main():
         group_detr=5,
         num_select=30,
         encoder='dinov2_windowed_tiny',
+        # encoder='dinov2_windowed_base',
+        
+        # patch_size=24,
+        # num_channels=1,
+        # eval=True,
+        # num_classes=32,
         num_classes=32,
         segmentation_head=True,
+
         pretrain_weights='output/xray_teeth33_dinov2tiny_small_seg/checkpoint0499.pth'
+        
     )
     
-    args = populate_args(**dict(config))
+    model = rf_detr.model.model
+    # rf_detr = RFDETRSmall(
     
+    def get_target_image_size(image_shape, rererence_width:int = 640):
+        ih, iw = image_shape[:2]
+        multiple = 16
+        
+        scale = rererence_width / iw
+        # else:
+            # scale = 
+        # ih = (ih * scale + multiple - 1) // multiple * multiple
+        ih = int(np.ceil(ih * scale / multiple) * multiple)
+        return (ih, rererence_width)
+        
 
-    # # device for export onnx
-    # # TODO: export onnx with cuda failed with onnx error
-    # device = torch.device("cpu")
-    # os.environ["CUDA_VISIBLE_DEVICES"] = device_id
-
-    # # fix the seed for reproducibility
-    # seed = args.seed + utils.get_rank()
-    # torch.manual_seed(seed)
-    # np.random.seed(seed)
-    # random.seed(seed)
+    def get_image_size(shape, stride=64):
+        h0, w0 = shape
+        size = (h0 // stride + 1) * stride, (w0 // stride + 1) * stride
+        return size
     
-    model = build_model(args)
-    checkpoint = torch.load(args.pretrain_weights, map_location='cpu', weights_only=False)
-    res = model.load_state_dict(checkpoint['model'], strict=False)
+    def resize_image(image, stride=64, target_width=640):
+        # size = get_image_size(image.shape[:2], stride=stride)
+        size = get_target_image_size(image.shape[:2], rererence_width=target_width)
+        size = get_image_size(size, stride=stride)
+        # return image.resize(size, Image.BILINEAR)
+        return cv2.resize(image, tuple(size[::-1]), interpolation=cv2.INTER_LINEAR)
+
+        # ''
+        
+        # next(rf_detr.model.model.parameters()).device
+    model = rf_detr.model.model
+    model.eval()
+        # "data/xray_teeth33/test/images/000000.png",]
+    from trainer import torch_utils
+    path = '/data1/jooyonglee/reverse_tomo/xray_panoramic/kaggle/Teeth Segmentation JSON/d2/img/'
+    found = glob.glob(f'{path}/*.jpg')
+    for file in found:
+        img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+        rsz_img = resize_image(img, stride=64)
+        img2 = np.repeat(rsz_img[None], 3, axis=0)
+        
+        img_tensor = torch_utils.data_convert(img2[None])
+        
+        with torch.no_grad():
+            outputs = model(img_tensor)
+            print(outputs.keys())
+            print(outputs['pred_logits'].shape, outputs['pred_boxes'].shape)
+            if 'pred_masks' in outputs:
+                print(outputs['pred_masks'].shape)
+        
+        
+            draw_preditions_boxes(
+                img_tensor, outputs
+            )
+    # model = build_model(args)
+    # checkpoint = torch.load(args.pretrain_weights, map_location='cpu', weights_only=False)
+    # res = model.load_state_dict(checkpoint['model'], strict=False)
     
     print(model)
 main()
